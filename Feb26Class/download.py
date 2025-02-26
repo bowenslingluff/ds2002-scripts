423f2af12fb02095b7b296af04fa0ba9eacf9718
Bowen Slingluff <bcslingluff@gmail.com>
2025-02-26 16:38:50 -0500

    fixed typo in download.py

--- Feb26Class/download.py
+++ Feb26Class/download.py
@@ -6,6 +6,6 @@ s3 = boto3.client('s3')
 
 BUCKET ='jdx7es-ds2002'
 OBJ_NAME = 'puppies.jpeg'
-FILE_NAME = 'BOTO3_DOWNLOADED.jpg
+FILE_NAME = 'BOTO3_DOWNLOADED.jpg'
 
 response = s3.download_file(BUCKET, OBJ_NAME, FILE_NAME)
